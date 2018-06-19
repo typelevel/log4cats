3ace7342dbfacb83ad5301f048e4c6bda6bc28ab
Chris Davenport <chris@christopherdavenport.tech>
2018-06-19 09:17:14 -0400

    Fix Implicit Summoning Methods

--- core/shared/src/main/scala/io/chrisdavenport/log4cats/SelfAwareStructuredLogger.scala
+++ core/shared/src/main/scala/io/chrisdavenport/log4cats/SelfAwareStructuredLogger.scala
@@ -1,3 +1,7 @@
 package io.chrisdavenport.log4cats
 
 trait SelfAwareStructuredLogger[F[_]] extends SelfAwareLogger[F] with StructuredLogger[F]
+
+object SelfAwareStructuredLogger {
+  def apply[F[_]](implicit ev: SelfAwareStructuredLogger[F]): SelfAwareStructuredLogger[F] = ev
+}
