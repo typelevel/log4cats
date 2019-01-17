90a67f2cc39b8a7638758ccac59df3d259a454ac
Christopher Davenport <Chris@ChristopherDavenport.tech>
2019-01-16 20:21:09 -0500

    Modifications to underlying Logging construction

--- core/shared/src/main/scala/io/chrisdavenport/log4cats/SelfAwareStructuredLogger.scala
+++ core/shared/src/main/scala/io/chrisdavenport/log4cats/SelfAwareStructuredLogger.scala
@@ -4,4 +4,47 @@ trait SelfAwareStructuredLogger[F[_]] extends SelfAwareLogger[F] with Structured
 
 object SelfAwareStructuredLogger {
   def apply[F[_]](implicit ev: SelfAwareStructuredLogger[F]): SelfAwareStructuredLogger[F] = ev
+
+  def withContext[F[_]](sl: SelfAwareStructuredLogger[F])(ctx: Map[String, String]): SelfAwareStructuredLogger[F] =
+    new ExtraContextSelfAwareStructuredLogger[F](sl)(ctx)
+
+  private class ExtraContextSelfAwareStructuredLogger[F[_]](sl: SelfAwareStructuredLogger[F])(ctx: Map[String, String])
+    extends SelfAwareStructuredLogger[F]{
+      private val outer = ctx
+      def error(message: => String): F[Unit] = sl.error(outer)(message)
+      def warn(message: => String): F[Unit] = sl.warn(outer)(message)
+      def info(message: => String): F[Unit] = sl.info(outer)(message)
+      def debug(message: => String): F[Unit] = sl.debug(outer)(message)
+      def trace(message: => String): F[Unit] = sl.trace(outer)(message)
+      def trace(ctx: Map[String,String])(msg: => String): F[Unit] =
+        sl.trace(outer ++ ctx)(msg)
+      def debug(ctx: Map[String, String])(msg: => String): F[Unit] = 
+        sl.debug(outer ++ ctx)(msg)
+      def info(ctx: Map[String, String])(msg: => String): F[Unit] = 
+        sl.info(outer ++ ctx)(msg)
+      def warn(ctx: Map[String, String])(msg: => String): F[Unit] = 
+        sl.warn(outer ++ ctx)(msg)
+      def error(ctx: Map[String, String])(msg: => String): F[Unit] =
+        sl.error(outer ++ ctx)(msg)
+
+      def isTraceEnabled: F[Boolean] = sl.isTraceEnabled
+      def isDebugEnabled: F[Boolean] = sl.isDebugEnabled
+      def isInfoEnabled: F[Boolean] = sl.isInfoEnabled
+      def isWarnEnabled: F[Boolean] = sl.isWarnEnabled
+      def isErrorEnabled: F[Boolean] = sl.isErrorEnabled
+
+      /** 
+       * Context Logging Is not available on throwable methods 
+       */
+      def error(t: Throwable)(message: => String): F[Unit] = 
+        sl.error(t)(message)
+      def warn(t: Throwable)(message: => String): F[Unit] = 
+        sl.warn(t)(message)
+      def info(t: Throwable)(message: => String): F[Unit] = 
+        sl.info(t)(message)
+      def debug(t: Throwable)(message: => String): F[Unit] = 
+        sl.debug(t)(message)
+      def trace(t: Throwable)(message: => String): F[Unit] =
+        sl.trace(t)(message)
+    }
 }
--- core/shared/src/main/scala/io/chrisdavenport/log4cats/StructuredLogger.scala
+++ core/shared/src/main/scala/io/chrisdavenport/log4cats/StructuredLogger.scala
@@ -1,22 +1,50 @@
 package io.chrisdavenport.log4cats
 
 trait StructuredLogger[F[_]] extends Logger[F] {
-  def trace(ctx: (String, String)*)(msg: => String): F[Unit]
-  def debug(ctx: (String, String)*)(msg: => String): F[Unit]
-  def info(ctx: (String, String)*)(msg: => String): F[Unit]
-  def warn(ctx: (String, String)*)(msg: => String): F[Unit]
-  def error(ctx: (String, String)*)(msg: => String): F[Unit]
+  def trace(ctx: Map[String,String])(msg: => String): F[Unit]
+  def debug(ctx: Map[String, String])(msg: => String): F[Unit]
+  def info(ctx: Map[String, String])(msg: => String): F[Unit]
+  def warn(ctx: Map[String, String])(msg: => String): F[Unit]
+  def error(ctx: Map[String, String])(msg: => String): F[Unit]
 }
 
 object StructuredLogger{
   def apply[F[_]](implicit ev: StructuredLogger[F]): StructuredLogger[F] = ev
 
-  def withContext[F[_]](sl: StructuredLogger[F])(ctx: (String, String)*): MessageLogger[F] =
-    new MessageLogger[F]{
-      def error(message: => String): F[Unit] = sl.error(ctx:_*)(message)
-      def warn(message: => String): F[Unit] = sl.warn(ctx:_*)(message)
-      def info(message: => String): F[Unit] = sl.info(ctx:_*)(message)
-      def debug(message: => String): F[Unit] = sl.debug(ctx:_*)(message)
-      def trace(message: => String): F[Unit] = sl.trace(ctx:_*)(message)
+  def withContext[F[_]](sl: StructuredLogger[F])(ctx: Map[String, String]): StructuredLogger[F] =
+    new ExtraContextStructuredLogger[F](sl)(ctx)
+
+  private class ExtraContextStructuredLogger[F[_]](sl: StructuredLogger[F])(ctx: Map[String, String])
+    extends StructuredLogger[F]{
+      private val outer = ctx
+      def error(message: => String): F[Unit] = sl.error(outer)(message)
+      def warn(message: => String): F[Unit] = sl.warn(outer)(message)
+      def info(message: => String): F[Unit] = sl.info(outer)(message)
+      def debug(message: => String): F[Unit] = sl.debug(outer)(message)
+      def trace(message: => String): F[Unit] = sl.trace(outer)(message)
+      def trace(ctx: Map[String,String])(msg: => String): F[Unit] =
+        sl.trace(outer ++ ctx)(msg)
+      def debug(ctx: Map[String, String])(msg: => String): F[Unit] = 
+        sl.debug(outer ++ ctx)(msg)
+      def info(ctx: Map[String, String])(msg: => String): F[Unit] = 
+        sl.info(outer ++ ctx)(msg)
+      def warn(ctx: Map[String, String])(msg: => String): F[Unit] = 
+        sl.warn(outer ++ ctx)(msg)
+      def error(ctx: Map[String, String])(msg: => String): F[Unit] =
+        sl.error(outer ++ ctx)(msg)
+
+      /** 
+       * Context Logging Is not available on throwable methods 
+       */
+      def error(t: Throwable)(message: => String): F[Unit] = 
+        sl.error(t)(message)
+      def warn(t: Throwable)(message: => String): F[Unit] = 
+        sl.warn(t)(message)
+      def info(t: Throwable)(message: => String): F[Unit] = 
+        sl.info(t)(message)
+      def debug(t: Throwable)(message: => String): F[Unit] = 
+        sl.debug(t)(message)
+      def trace(t: Throwable)(message: => String): F[Unit] =
+        sl.trace(t)(message)
     }
 }
