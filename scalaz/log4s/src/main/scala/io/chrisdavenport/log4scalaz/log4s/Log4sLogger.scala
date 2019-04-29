e26b78a13f3049c3d1607e5abb0ca28cec061b8f
Lorand Szakacs <lsz@lorandszakacs.com>
2019-04-29 17:05:57 +0300

    Replace zio IO[Nothing, ?] with UIO[?] alias

--- scalaz/log4s/src/main/scala/io/chrisdavenport/log4scalaz/log4s/Log4sLogger.scala
+++ scalaz/log4s/src/main/scala/io/chrisdavenport/log4scalaz/log4s/Log4sLogger.scala
@@ -2,50 +2,39 @@ package io.chrisdavenport.log4cats.log4s
 
 import io.chrisdavenport.log4cats._
 import org.log4s.{Logger => Base}
-import scalaz.zio.IO
+import scalaz.zio.UIO
 
 object Log4sLogger {
 
-  def createLocal = fromLog4s(org.log4s.getLogger)
-  def createByName(name: String) = fromLog4s(org.log4s.getLogger(name))
-  def createByClass(clazz: Class[_]) = fromLog4s(org.log4s.getLogger(clazz))
+  def createLocal: SelfAwareLogger[UIO] = fromLog4s(org.log4s.getLogger)
+  def createByName(name: String): SelfAwareLogger[UIO] = fromLog4s(org.log4s.getLogger(name))
+  def createByClass(clazz: Class[_]): SelfAwareLogger[UIO] = fromLog4s(org.log4s.getLogger(clazz))
 
-  def fromLog4s(logger: Base): SelfAwareLogger[IO[Nothing, ?]] =
-    new SelfAwareLogger[IO[Nothing, ?]] {
-      override def isTraceEnabled: IO[Nothing, Boolean] =
-        IO.sync(logger.isTraceEnabled)
-      override def isDebugEnabled: IO[Nothing, Boolean] =
-        IO.sync(logger.isDebugEnabled)
-      override def isInfoEnabled: IO[Nothing, Boolean] =
-        IO.sync(logger.isInfoEnabled)
-      override def isWarnEnabled: IO[Nothing, Boolean] =
-        IO.sync(logger.isWarnEnabled)
-      override def isErrorEnabled: IO[Nothing, Boolean] =
-        IO.sync(logger.isErrorEnabled)
+  def fromLog4s(logger: Base): SelfAwareLogger[UIO] = new SelfAwareLogger[UIO] {
+    override def isTraceEnabled: UIO[Boolean] = UIO.apply(logger.isTraceEnabled)
+    override def isDebugEnabled: UIO[Boolean] = UIO.apply(logger.isDebugEnabled)
+    override def isInfoEnabled: UIO[Boolean] = UIO.apply(logger.isInfoEnabled)
+    override def isWarnEnabled: UIO[Boolean] = UIO.apply(logger.isWarnEnabled)
+    override def isErrorEnabled: UIO[Boolean] = UIO.apply(logger.isErrorEnabled)
 
-      override def error(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.error(message))
-      override def error(t: Throwable)(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.error(t)(message))
+    override def error(message: => String): UIO[Unit] = UIO.apply(logger.error(message))
+    override def error(t: Throwable)(message: => String): UIO[Unit] =
+      UIO.apply(logger.error(t)(message))
 
-      override def warn(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.warn(message))
-      override def warn(t: Throwable)(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.warn(t)(message))
+    override def warn(message: => String): UIO[Unit] = UIO.apply(logger.warn(message))
+    override def warn(t: Throwable)(message: => String): UIO[Unit] =
+      UIO.apply(logger.warn(t)(message))
 
-      override def info(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.info(message))
-      override def info(t: Throwable)(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.info(t)(message))
+    override def info(message: => String): UIO[Unit] = UIO.apply(logger.info(message))
+    override def info(t: Throwable)(message: => String): UIO[Unit] =
+      UIO.apply(logger.info(t)(message))
 
-      override def debug(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.debug(message))
-      override def debug(t: Throwable)(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.debug(t)(message))
+    override def debug(message: => String): UIO[Unit] = UIO.apply(logger.debug(message))
+    override def debug(t: Throwable)(message: => String): UIO[Unit] =
+      UIO.apply(logger.debug(t)(message))
 
-      override def trace(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.trace(message))
-      override def trace(t: Throwable)(message: => String): IO[Nothing, Unit] =
-        IO.sync(logger.trace(t)(message))
-    }
+    override def trace(message: => String): UIO[Unit] = UIO.apply(logger.trace(message))
+    override def trace(t: Throwable)(message: => String): UIO[Unit] =
+      UIO.apply(logger.trace(t)(message))
+  }
 }
