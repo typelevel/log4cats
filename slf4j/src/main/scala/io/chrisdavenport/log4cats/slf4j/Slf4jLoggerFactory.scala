c9a7bd7e08abd1bbd1b764af0e271e5852f2aedc
Vitaly Lavrov <lavrovvv@gmail.com>
2019-09-23 21:39:13 +0200

    Make Slf4jLoggerFactory private

--- slf4j/src/main/scala/io/chrisdavenport/log4cats/slf4j/Slf4jLoggerFactory.scala
+++ slf4j/src/main/scala/io/chrisdavenport/log4cats/slf4j/Slf4jLoggerFactory.scala
@@ -3,15 +3,16 @@ package io.chrisdavenport.log4cats.slf4j
 import cats.effect.Sync
 import io.chrisdavenport.log4cats.{LoggerFactory, SelfAwareStructuredLogger}
 
-class Slf4jLoggerFactory[F[_]: Sync] extends LoggerFactory[F] {
-
-  override def fromName(name: String): F[SelfAwareStructuredLogger[F]] =
-    Slf4jLogger.fromName(name)
-
-  override def fromClass(clazz: Class[_]): F[SelfAwareStructuredLogger[F]] =
-    Slf4jLogger.fromClass(clazz)
-}
-
 object Slf4jLoggerFactory {
-  def apply[F[_]: Sync]: Slf4jLoggerFactory[F] = new Slf4jLoggerFactory[F]
+
+  def apply[F[_]: Sync]: LoggerFactory[F] = new Slf4jLoggerFactory[F]
+
+  private class Slf4jLoggerFactory[F[_]: Sync] extends LoggerFactory[F] {
+
+    override def fromName(name: String): F[SelfAwareStructuredLogger[F]] =
+      Slf4jLogger.fromName(name)
+
+    override def fromClass(clazz: Class[_]): F[SelfAwareStructuredLogger[F]] =
+      Slf4jLogger.fromClass(clazz)
+  }
 }
