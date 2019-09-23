b94182b17ece77aa4327443698d511dc940b31df
Vitaly Lavrov <lavrovvv@gmail.com>
2019-09-23 21:44:31 +0200

    LoggerFactory implicit term resolution

--- core/shared/src/main/scala/io/chrisdavenport/log4cats/LoggerFactory.scala
+++ core/shared/src/main/scala/io/chrisdavenport/log4cats/LoggerFactory.scala
@@ -5,5 +5,9 @@ trait LoggerFactory[F[_]] {
   def fromName(name: String): F[SelfAwareStructuredLogger[F]]
 
   def fromClass(clazz: Class[_]): F[SelfAwareStructuredLogger[F]]
+}
 
+object LoggerFactory {
+
+  def apply[F[_]](implicit ev: LoggerFactory[F]): LoggerFactory[F] = ev
 }
