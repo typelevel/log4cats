90f81e1b8fc3e6bbf8d35d1295693cb56605e016
Diego E. Alonso Blas <diesalbla@gmail.com>
2020-02-20 16:29:44 +0000

    Log4Cats: Backport some API methods from newer versions.
    We bring back from the future version (1.0.0) some of the methods that
    already exist there, and which we can implement in current version.

--- core/shared/src/main/scala/io/chrisdavenport/log4cats/SelfAwareLogger.scala
+++ core/shared/src/main/scala/io/chrisdavenport/log4cats/SelfAwareLogger.scala
@@ -3,7 +3,7 @@ package io.chrisdavenport.log4cats
 trait SelfAwareLogger[F[_]] extends Logger[F] {
   def isTraceEnabled: F[Boolean]
   def isDebugEnabled: F[Boolean]
-  def isInfoEnabled: F[Boolean] 
+  def isInfoEnabled: F[Boolean]
   def isWarnEnabled: F[Boolean]
   def isErrorEnabled: F[Boolean]
 }
