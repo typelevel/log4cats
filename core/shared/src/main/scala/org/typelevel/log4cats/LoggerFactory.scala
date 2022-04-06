d2c3d64a014bbb06a753992fd4c608b2e8947087
Erlend Hamnaberg <erlend@hamnaberg.net>
2022-04-06 08:58:45 +0200

    Revert "The Companion trait did not work as expected"
    This reverts commit 7167e54a9c2e0a089154d7cda469f33a0797fc4f.

--- core/shared/src/main/scala/org/typelevel/log4cats/LoggerFactory.scala
+++ core/shared/src/main/scala/org/typelevel/log4cats/LoggerFactory.scala
@@ -18,7 +18,39 @@ package org.typelevel.log4cats
 
 import scala.annotation.implicitNotFound
 
-@implicitNotFound("""Implicit not found for LoggerFactory[${F}].
- Information can be found here: https://log4cats.github.io/logging-capability.html
-""")
+@implicitNotFound("""|
+                     |Implicit not found for LoggerFactory[${F}].
+                     |
+                     |Information can be found here: https://log4cats.github.io/logging-capability.html
+                     |""")
 trait LoggerFactory[F[_]] extends LoggerFactoryGen[F, SelfAwareStructuredLogger[F]]
+object LoggerFactory extends LoggerFactoryCompanion
+
+private[log4cats] trait LoggerFactoryCompanion {
+  def getLogger[F[_]](implicit
+      lf: LoggerFactory[F],
+      name: LoggerName
+  ): SelfAwareStructuredLogger[F] =
+    lf.getLogger
+  def getLoggerFromName[F[_]](name: String)(implicit
+      lf: LoggerFactory[F]
+  ): SelfAwareStructuredLogger[F] =
+    lf.getLoggerFromName(name)
+
+  def getLoggerFromClass[F[_]](clazz: Class[_])(implicit
+      lf: LoggerFactory[F]
+  ): SelfAwareStructuredLogger[F] =
+    lf.getLoggerFromClass(clazz)
+
+  def create[F[_]](implicit
+      lf: LoggerFactory[F],
+      name: LoggerName
+  ): F[SelfAwareStructuredLogger[F]] =
+    lf.create
+  def fromName[F[_]](name: String)(implicit lf: LoggerFactory[F]): F[SelfAwareStructuredLogger[F]] =
+    lf.fromName(name)
+  def fromClass[F[_]](clazz: Class[_])(implicit
+      lf: LoggerFactory[F]
+  ): F[SelfAwareStructuredLogger[F]] =
+    lf.fromClass(clazz)
+}
