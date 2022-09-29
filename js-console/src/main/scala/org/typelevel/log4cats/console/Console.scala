f64ad8e55626d26ca67d36040c67c4daf68d29ae
Brian Holt <bholt@dwolla.com>
2022-09-29 16:51:51 -0500

    add the copyright headers required by the project
    AFAIK there's no legal entity called "Typelevel" that owns the copyright for
    these contributions, so it's a little unclear what these refer to 🤷

--- js-console/src/main/scala/org/typelevel/log4cats/console/Console.scala
+++ js-console/src/main/scala/org/typelevel/log4cats/console/Console.scala
@@ -1,9 +1,26 @@
-/** All documentation for facades is thanks to Mozilla Contributors at https://developer.mozilla.org/en-US/docs/Web/API
+/*
+ * Copyright 2018 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* All documentation for facades is thanks to Mozilla Contributors at https://developer.mozilla.org/en-US/docs/Web/API
  * and available under the Creative Commons Attribution-ShareAlike v2.5 or later.
  * http://creativecommons.org/licenses/by-sa/2.5/
  *
  * Everything else is under the MIT License http://opensource.org/licenses/MIT
  */
+
 package org.typelevel.log4cats.console
 
 import scala.annotation.nowarn
