3240c588ca85cb4699b816cfa846b0a0115d7f47
Brian Holt <bholt@dwolla.com>
2022-12-05 15:16:56 -0600

    mark ConsoleF as package private to remove it from the public interface

--- js-console/src/main/scala/org/typelevel/log4cats/console/ConsoleF.scala
+++ js-console/src/main/scala/org/typelevel/log4cats/console/ConsoleF.scala
@@ -19,7 +19,7 @@ package console
 
 import cats.effect.kernel.Sync
 
-class ConsoleF[F[_]: Sync] {
+private[console] class ConsoleF[F[_]: Sync] {
   def info(message: Any, optionalParams: Any*): F[Unit] =
     Sync[F].delay(Console.info(message, optionalParams: _*))
   def warn(message: Any, optionalParams: Any*): F[Unit] =
@@ -30,7 +30,7 @@ class ConsoleF[F[_]: Sync] {
     Sync[F].delay(Console.debug(message, optionalParams: _*))
 }
 
-object ConsoleF {
+private[console] object ConsoleF {
   def apply[F[_]: ConsoleF]: ConsoleF[F] = implicitly
 
   implicit def syncInstance[F[_]: Sync]: ConsoleF[F] = new ConsoleF[F]
