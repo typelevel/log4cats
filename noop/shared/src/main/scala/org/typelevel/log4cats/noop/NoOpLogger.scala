373a21315ee52db25c9128c393507854d65b91c9
Antoine Comte <antoine@comte.cc>
2023-03-26 12:17:47 +0200

    Separate lazy and strict implementation

--- noop/shared/src/main/scala/org/typelevel/log4cats/noop/NoOpLogger.scala
+++ noop/shared/src/main/scala/org/typelevel/log4cats/noop/NoOpLogger.scala
@@ -28,44 +28,83 @@ object NoOpLogger {
   def impl[F[_]: Applicative] = impl_[F](evaluateArgs = false)
 
   private[noop] def impl_[F[_]: Applicative](evaluateArgs: Boolean): SelfAwareStructuredLogger[F] =
-    new SelfAwareStructuredLogger[F] {
+    if (evaluateArgs) stricImpl else lazyImpl
 
-      val no: F[Boolean] = Applicative[F].pure(false)
-      def void(arg: => Any): F[Unit] = Applicative[F].pure {
-        if (evaluateArgs) {
-          val _ = arg
-          ()
-        } else ()
-      }
-      @inline override def isTraceEnabled: F[Boolean] = no
-      @inline override def isDebugEnabled: F[Boolean] = no
-      @inline override def isInfoEnabled: F[Boolean] = no
-      @inline override def isWarnEnabled: F[Boolean] = no
-      @inline override def isErrorEnabled: F[Boolean] = no
-      @inline override def trace(t: Throwable)(msg: => String): F[Unit] = void(t)
-      @inline override def trace(msg: => String): F[Unit] = void(msg)
-      @inline override def trace(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
-      @inline override def debug(t: Throwable)(msg: => String): F[Unit] = void(msg)
-      @inline override def debug(msg: => String): F[Unit] = void(msg)
-      @inline override def debug(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
-      @inline override def info(t: Throwable)(msg: => String): F[Unit] = void(msg)
-      @inline override def info(msg: => String): F[Unit] = void(msg)
-      @inline override def info(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
-      @inline override def warn(t: Throwable)(msg: => String): F[Unit] = void(msg)
-      @inline override def warn(msg: => String): F[Unit] = void(msg)
-      @inline override def warn(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
-      @inline override def error(t: Throwable)(msg: => String): F[Unit] = void(msg)
-      @inline override def error(msg: => String): F[Unit] = void(msg)
-      @inline override def error(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
-      @inline override def trace(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
-        void(msg)
-      @inline override def debug(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
-        void(msg)
-      @inline override def info(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
-        void(msg)
-      @inline override def warn(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
-        void(msg)
-      @inline override def error(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
-        void(msg)
+  private def lazyImpl[F[_]: Applicative] = new SelfAwareStructuredLogger[F] {
+
+    val no: F[Boolean] = Applicative[F].pure(false)
+    val unit: F[Unit] = Applicative[F].pure(())
+
+    @inline override def isTraceEnabled: F[Boolean] = no
+    @inline override def isDebugEnabled: F[Boolean] = no
+    @inline override def isInfoEnabled: F[Boolean] = no
+    @inline override def isWarnEnabled: F[Boolean] = no
+    @inline override def isErrorEnabled: F[Boolean] = no
+    @inline override def trace(t: Throwable)(msg: => String): F[Unit] = unit
+    @inline override def trace(msg: => String): F[Unit] = unit
+    @inline override def trace(ctx: Map[String, String])(msg: => String): F[Unit] = unit
+    @inline override def debug(t: Throwable)(msg: => String): F[Unit] = unit
+    @inline override def debug(msg: => String): F[Unit] = unit
+    @inline override def debug(ctx: Map[String, String])(msg: => String): F[Unit] = unit
+    @inline override def info(t: Throwable)(msg: => String): F[Unit] = unit
+    @inline override def info(msg: => String): F[Unit] = unit
+    @inline override def info(ctx: Map[String, String])(msg: => String): F[Unit] = unit
+    @inline override def warn(t: Throwable)(msg: => String): F[Unit] = unit
+    @inline override def warn(msg: => String): F[Unit] = unit
+    @inline override def warn(ctx: Map[String, String])(msg: => String): F[Unit] = unit
+    @inline override def error(t: Throwable)(msg: => String): F[Unit] = unit
+    @inline override def error(msg: => String): F[Unit] = unit
+    @inline override def error(ctx: Map[String, String])(msg: => String): F[Unit] = unit
+    @inline override def trace(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      unit
+    @inline override def debug(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      unit
+    @inline override def info(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      unit
+    @inline override def warn(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      unit
+    @inline override def error(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      unit
+  }
+
+  private def stricImpl[F[_]: Applicative] = new SelfAwareStructuredLogger[F] {
+
+    val no: F[Boolean] = Applicative[F].pure(false)
+    def void(arg: => Any): F[Unit] = Applicative[F].pure {
+      val _ = arg
+      ()
     }
+
+    @inline override def isTraceEnabled: F[Boolean] = no
+    @inline override def isDebugEnabled: F[Boolean] = no
+    @inline override def isInfoEnabled: F[Boolean] = no
+    @inline override def isWarnEnabled: F[Boolean] = no
+    @inline override def isErrorEnabled: F[Boolean] = no
+    @inline override def trace(t: Throwable)(msg: => String): F[Unit] = void(t)
+    @inline override def trace(msg: => String): F[Unit] = void(msg)
+    @inline override def trace(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
+    @inline override def debug(t: Throwable)(msg: => String): F[Unit] = void(msg)
+    @inline override def debug(msg: => String): F[Unit] = void(msg)
+    @inline override def debug(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
+    @inline override def info(t: Throwable)(msg: => String): F[Unit] = void(msg)
+    @inline override def info(msg: => String): F[Unit] = void(msg)
+    @inline override def info(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
+    @inline override def warn(t: Throwable)(msg: => String): F[Unit] = void(msg)
+    @inline override def warn(msg: => String): F[Unit] = void(msg)
+    @inline override def warn(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
+    @inline override def error(t: Throwable)(msg: => String): F[Unit] = void(msg)
+    @inline override def error(msg: => String): F[Unit] = void(msg)
+    @inline override def error(ctx: Map[String, String])(msg: => String): F[Unit] = void(msg)
+    @inline override def trace(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      void(msg)
+    @inline override def debug(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      void(msg)
+    @inline override def info(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      void(msg)
+    @inline override def warn(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      void(msg)
+    @inline override def error(ctx: Map[String, String], t: Throwable)(msg: => String): F[Unit] =
+      void(msg)
+  }
+
 }
