1ce07a526f863b9408e2626916b1f81e791621b9
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-03-28 10:16:58 +0530

    Update IOLocalHelpers with naming

--- ce3/shared/src/main/scala/org/typelevel/log4cats/ce3/IOLocalHelpers.scala
+++ ce3/shared/src/main/scala/org/typelevel/log4cats/ce3/IOLocalHelpers.scala
@@ -5,12 +5,12 @@ import cats.mtl.Local
 import org.typelevel.log4cats.{LoggerFactory, SelfAwareStructuredLogger}
 
 object IOLocalHelpers {
-  def loggerWithContextFromIOLocal(
+  def loggerWithContext(
       sl: SelfAwareStructuredLogger[IO]
       )(implicit local: Local[IO, Map[String, String]]): SelfAwareStructuredLogger[IO] =
     SelfAwareStructuredLogger.withContextF(sl)(local.ask)
 
-  def factoryWithContextFromIOLocal(
+  def factoryWithContext(
       lf: LoggerFactory[IO]
       )(implicit local: Local[IO, Map[String, String]]): LoggerFactory[IO] =
     LoggerFactory.withContextF(lf)(local.ask)
