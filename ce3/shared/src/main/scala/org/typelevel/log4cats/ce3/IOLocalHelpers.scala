9e42efd925a7a6429182932f84a6fed8b2a69e49
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-03-28 08:03:46 +0530

    added cats.mtl.Local usage

--- ce3/shared/src/main/scala/org/typelevel/log4cats/ce3/IOLocalHelpers.scala
+++ ce3/shared/src/main/scala/org/typelevel/log4cats/ce3/IOLocalHelpers.scala
@@ -1,16 +1,17 @@
 package org.typelevel.log4cats.ce3
 
-import cats.effect.{IO, IOLocal}
+import cats.effect.IO
+import cats.mtl.Local
 import org.typelevel.log4cats.{LoggerFactory, SelfAwareStructuredLogger}
 
 object IOLocalHelpers {
   def loggerWithContextFromIOLocal(
-      sl: SelfAwareStructuredLogger[IO],
-      local: IOLocal[Map[String, String]]
-  ): SelfAwareStructuredLogger[IO] = SelfAwareStructuredLogger.withContextF(sl)(local.get)
+                                    sl: SelfAwareStructuredLogger[IO]
+                                  )(implicit local: Local[IO, Map[String, String]]): SelfAwareStructuredLogger[IO] =
+    SelfAwareStructuredLogger.withContextF(sl)(local.ask)
 
   def factoryWithContextFromIOLocal(
-      lf: LoggerFactory[IO],
-      local: IOLocal[Map[String, String]]
-  ): LoggerFactory[IO] = LoggerFactory.withContextF(lf)(local.get)
+                                     lf: LoggerFactory[IO]
+                                   )(implicit local: Local[IO, Map[String, String]]): LoggerFactory[IO] =
+    LoggerFactory.withContextF(lf)(local.ask)
 }
