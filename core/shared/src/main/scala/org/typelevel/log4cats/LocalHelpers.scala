489d29ecbd632e3147518fa9a2eea6c6c747a558
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-04-08 00:20:15 +0530

    handled imports

--- core/shared/src/main/scala/org/typelevel/log4cats/LocalHelpers.scala
+++ core/shared/src/main/scala/org/typelevel/log4cats/LocalHelpers.scala
@@ -18,7 +18,6 @@ package org.typelevel.log4cats
 
 import cats.FlatMap
 import cats.mtl.Local
-import org.typelevel.log4cats.{LoggerFactory, SelfAwareStructuredLogger}
 
 object LocalHelpers {
   implicit class LoggerOps[F[_]](sl: SelfAwareStructuredLogger[F]) {
