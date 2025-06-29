ac5aaee33d58307605ff7750ce9f523416387ea2
Jay-Lokhande <jaylokhande2711@gmail.com>
2025-06-29 10:51:10 +0000

    modify log with parameterized with the context type

--- core/shared/src/main/scala/org/typelevel/log4cats/Log.scala
+++ core/shared/src/main/scala/org/typelevel/log4cats/Log.scala
@@ -16,8 +16,7 @@
 
 package org.typelevel.log4cats
 
-import scala.collection.{Map => MapLike}
-import scala.collection.mutable.{Map => MMap}
+import scala.collection.mutable
 import scala.concurrent.duration.FiniteDuration
 
 /**
@@ -26,130 +25,132 @@ import scala.concurrent.duration.FiniteDuration
  * (or by running an effect). Elements such as timestamps should be provided by means of
  * middlewares.
  */
-trait Log {
+trait Log[Ctx] {
   def timestamp: Option[FiniteDuration]
   def level: KernelLogLevel
-  def levelValue: Double
+  def levelValue: Int
   def message: String
   def throwable: Option[Throwable]
-  def context: Map[String, Context]
+  def context: Map[String, Ctx]
   def fileName: Option[String]
   def className: Option[String]
   def methodName: Option[String]
   def line: Option[Int]
 
   def unsafeThrowable: Throwable
-  def unsafeContext: MapLike[String, Context]
+  def unsafeContext: Map[String, Ctx]
 }
 
 object Log {
-  trait Builder {
-    def withTimestamp(value: FiniteDuration): Builder
-    def withLevel(level: KernelLogLevel): Builder
-    def withLevelValue(levelValue: Double): Builder
-    def withMessage(message: => String): Builder
-    def withThrowable(throwable: Throwable): Builder
-    def withContext(name: String)(f: Context): Builder
-    def withFileName(name: String): Builder
-    def withClassName(name: String): Builder
-    def withLine(line: Int): Builder
+  trait Builder[Ctx] {
+    def withTimestamp(value: FiniteDuration): Builder[Ctx]
+    def withLevel(level: KernelLogLevel): Builder[Ctx]
+    def withLevelValue(levelValue: Int): Builder[Ctx]
+    def withMessage(message: => String): Builder[Ctx]
+    def withThrowable(throwable: Throwable): Builder[Ctx]
+    def withContext(name: String)(f: Ctx): Builder[Ctx]
+    def withFileName(name: String): Builder[Ctx]
+    def withClassName(name: String): Builder[Ctx]
+    def withLine(line: Int): Builder[Ctx]
 
     final def withContextMap[A: Context.Encoder](
-        mdc: Map[String, A]
-    ): Builder = {
+        contextMap: Map[String, A]
+    )(implicit ev: A =:= Ctx): Builder[Ctx] = {
       var builder = this
-      mdc.foreach { case (k, v) =>
-        builder = withContext(k)(v)
+      contextMap.foreach { case (k, v) =>
+        builder = withContext(k)(ev(v))
       }
       builder
     }
 
-    def build(): Log
+    def build(): Log[Ctx]
   }
 
-  def mutableBuilder(): Builder = new MutableBuilder()
+  def mutableBuilder[Ctx](): Builder[Ctx] = new MutableBuilder[Ctx]()
 
-  private class MutableBuilder private[Log] () extends Builder with Log {
-    def build(): Log = this
+  private class MutableBuilder[Ctx] private[Log] () extends Builder[Ctx] with Log[Ctx] {
+    private var _timestamp: Option[FiniteDuration] = None
+    private var _level: Option[KernelLogLevel] = None
+    private var _levelValue: Option[Int] = None
+    private var _message: Option[String] = None
+    private var _throwable: Option[Throwable] = None
+    private var _context: Option[mutable.Map[String, Ctx]] = None
+    private var _fileName: Option[String] = None
+    private var _className: Option[String] = None
+    private var _methodName: Option[String] = None
+    private var _line: Option[Int] = None
 
-    def timestamp: Option[FiniteDuration] = Option(_timestamp)
-    def level: KernelLogLevel = if (_level == null) KernelLogLevel.Debug else _level
-    def levelValue: Double =
-      if (_levelValue < 0) level.value else _levelValue
-    def message: String = if (_message == null) "" else _message
-    def throwable: Option[Throwable] = Option(_throwable)
-    def context: Map[String, Context] =
-      if (_context == null) Map.empty else _context.toMap
+    def build(): Log[Ctx] = this
 
-    def className: Option[String] = Option(_className)
-    def fileName: Option[String] = Option(_fileName)
-    def methodName: Option[String] = Option(_methodName)
-    def line: Option[Int] = Some(_line).filter(_ > 0)
+    def timestamp: Option[FiniteDuration] = _timestamp
+    def level: KernelLogLevel = _level.getOrElse(KernelLogLevel.Debug)
+    def levelValue: Int =
+      _levelValue.getOrElse(level.value)
+    def message: String = _message.getOrElse("")
+    def throwable: Option[Throwable] = _throwable
+    def context: Map[String, Ctx] =
+      _context.map(_.toMap).getOrElse(Map.empty)
 
-    def unsafeThrowable: Throwable = _throwable
-    def unsafeContext: MapLike[String, Context] = _context
+    def className: Option[String] = _className
+    def fileName: Option[String] = _fileName
+    def methodName: Option[String] = _methodName
+    def line: Option[Int] = _line.filter(_ > 0)
 
-    private var _timestamp: FiniteDuration = null
-    private var _level: KernelLogLevel = null
-    private var _levelValue: Double = -1
-    private var _message: String = null
-    private var _throwable: Throwable = null
-    private var _context: MMap[String, Context] = null
-    private var _fileName: String = null
-    private var _className: String = null
-    private var _methodName: String = null
-    private var _line: Int = -1
+    def unsafeThrowable: Throwable = _throwable.get
+    def unsafeContext: Map[String, Ctx] = _context.get.toMap
 
     def withTimestamp(value: FiniteDuration): this.type = {
-      this._timestamp = value
+      this._timestamp = Some(value)
       this
     }
 
     def withLevel(level: KernelLogLevel): this.type = {
-      this._level = level
+      this._level = Some(level)
       this
     }
 
-    def withLevelValue(levelValue: Double): this.type = {
-      this._levelValue = levelValue
+    def withLevelValue(levelValue: Int): this.type = {
+      this._levelValue = Some(levelValue)
       this
     }
 
     def withMessage(message: => String): this.type = {
-      this._message = message
+      this._message = Some(message)
       this
     }
 
     def withThrowable(throwable: Throwable): this.type = {
-      this._throwable = throwable
+      this._throwable = Some(throwable)
       this
     }
 
-    def withContext(name: String)(value: Context): this.type = {
-      if (this._context == null) {
-        this._context = MMap.empty[String, Context]
+    def withContext(name: String)(value: Ctx): this.type = {
+      val map = _context.getOrElse {
+        val newMap = mutable.Map.empty[String, Ctx]
+        this._context = Some(newMap)
+        newMap
       }
-      this._context += name -> value
+      map += name -> value
       this
     }
 
     def withFileName(name: String): this.type = {
-      this._fileName = name
+      this._fileName = Some(name)
       this
     }
 
     def withClassName(name: String): this.type = {
-      this._className = name
+      this._className = Some(name)
       this
     }
 
     def withLine(line: Int): this.type = {
-      this._line = line
+      this._line = Some(line)
       this
     }
 
     def withMethodName(name: String): this.type = {
-      this._methodName = name
+      this._methodName = Some(name)
       this
     }
   }
